8946eebf327bcf047763df87844ed7d4d396b891
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2025-11-13 22:29:09 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# reMarkable-nix!
+Declarative packaging for the reMarkable desktop app - using nix flakes + wrapWine by [lucasew](https://github.com/lucasew/nixcfg/blob/e542e743774f499f996a4f886a8d4a4133fce258/packages/wrapWine.nix). Since it's packaged with nix, it will just work!
+
+## Usage
+### Standalone
+```
+```bash
+git clone https://github.com/YOUR_USERNAME/reMarkable-nix.git
+cd reMarkable-nix
+nix build
+./result/bin/remarkable
+```
+
+### NixOS Configuration
+
+```
+# in your flake.nix
+inputs = {
+  # your other inputs
+  remarkable.url = "github:ewtodd/reMarkable-nix";
+};
+# ...
+
+# in your configuration.nix
+{ inputs, ... }: 
+let 
+  remarkable = inputs.remarkable.packages."x86_64-linux".default;
+in {
+  # ...
+  environment.systemPackages = [ remarkable ];
+  # ...
+}
+```
---
+++ flake.nix
@@ -0,0 +1,63 @@
+
+{
+  description = "reMarkable Desktop App";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        wrapWine = ((import ./wrapWine.nix) { inherit pkgs; }).wrapWine;
+        installer = builtins.fetchurl {
+          url = "https://downloads.remarkable.com/latest/windows";
+          sha256 = "sha256:0wjkckls47ln3clqnzh162y1lf8p06sj7rd3pz2lldwpqghkcdqk";
+        };
+        wine = pkgs.wineWowPackages.stagingFull;
+        remarkable_bin = wrapWine {
+          wine = wine;
+          name = "remarkable";
+          is64bits = true;  # Keep as false since wrapWine.nix has the wine64 path issue
+          executable = "$WINEPREFIX/drive_c/reMarkable/reMarkable.exe";
+          chdir = "$WINEPREFIX/drive_c/reMarkable";
+          firstrunScript = ''
+            echo "Installing reMarkable Desktop App..."
+            
+            ${wine}/bin/wine ${installer} install \
+              --root "C:\\reMarkable" \
+              --accept-licenses \
+              --default-answer \
+              --confirm-command
+            
+            sleep 10
+            wineserver -w
+            
+            if [ -f "$WINEPREFIX/drive_c/reMarkable/reMarkable.exe" ]; then
+              echo "Installation successful!"
+            else
+              echo "ERROR: reMarkable.exe not found"
+              exit 1
+            fi
+          '';
+        };
+        remarkable_desktop = pkgs.makeDesktopItem {
+          name = "reMarkable";
+          desktopName = "reMarkable Desktop";
+          type = "Application";
+          exec = "${remarkable_bin}/bin/remarkable";
+        };
+        remarkable = pkgs.symlinkJoin {
+          name = "remarkable";
+          paths = [ remarkable_bin remarkable_desktop ];
+        };
+      in {
+        packages = {
+          remarkable = remarkable;
+          default = remarkable;
+        };
+      });
+}
+
