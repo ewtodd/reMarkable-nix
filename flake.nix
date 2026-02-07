9aca287a792da5266394d46154ceadf8230eeb37
Ethan Todd <30243637+ewtodd@users.noreply.github.com>
2026-02-06 22:43:56 -0500

    update hash

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "reMarkable Desktop App";
 
@@ -7,34 +6,36 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
         wrapWine = ((import ./wrapWine.nix) { inherit pkgs; }).wrapWine;
         installer = builtins.fetchurl {
           url = "https://downloads.remarkable.com/latest/windows";
-          sha256 = "sha256:169sw60rn3rhkbbxgyc3cny3sq98h2bsgm98kzf8m3zz9bwblpzc";
+          sha256 = "sha256:062d77y09adlmbqyzykdfkcdfamxdqzwdfgz1fcpnk9jcqkk4c87";
         };
         wine = pkgs.wineWowPackages.stagingFull;
         remarkable_bin = wrapWine {
           wine = wine;
           name = "remarkable";
-          is64bits = true;  # Keep as false since wrapWine.nix has the wine64 path issue
+          is64bits = true; # Keep as false since wrapWine.nix has the wine64 path issue
           executable = "$WINEPREFIX/drive_c/reMarkable/reMarkable.exe";
           chdir = "$WINEPREFIX/drive_c/reMarkable";
           firstrunScript = ''
             echo "Installing reMarkable Desktop App..."
-            
+
             ${wine}/bin/wine ${installer} install \
               --root "C:\\reMarkable" \
               --accept-licenses \
               --default-answer \
               --confirm-command
-            
+
             sleep 10
             wineserver -w
-            
+
             if [ -f "$WINEPREFIX/drive_c/reMarkable/reMarkable.exe" ]; then
               echo "Installation successful!"
             else
@@ -51,13 +52,17 @@
         };
         remarkable = pkgs.symlinkJoin {
           name = "remarkable";
-          paths = [ remarkable_bin remarkable_desktop ];
+          paths = [
+            remarkable_bin
+            remarkable_desktop
+          ];
         };
-      in {
+      in
+      {
         packages = {
           remarkable = remarkable;
           default = remarkable;
         };
-      });
+      }
+    );
 }
-
